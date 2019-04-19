39735dd9f19cae133ff2755081dc4c281e5b62cb
Ellen Marie Dash <me@duckie.co>
2019-04-19 18:58:39 -0400

    Move script to external file. I wasn't even aware you could have it inlined....

--- src/levels/ProgrammaticLevel.gd
+++ src/levels/ProgrammaticLevel.gd
@@ -1,15 +1,66 @@
 extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
+var mobs = []
+var grids
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	grids = $Grids
+
+	var Floor = new_grid(Vector3(0, 0, 0), Vector3(0, 0, 0))
+	var Ceil = new_grid(Vector3(0, 50, 0), Vector3(0, 180, 180))
+	#var Left = new_grid(Vector3(0, 0, -100), Vector3(90, 180, 180))
+	#var Right = new_grid(Vector3(0, 0, 100), Vector3(-90, 180, 180))
+	#var Start = new_grid(Vector3(-10, 0, 0), Vector3(-90, 180, 90))
+	#var End = new_grid(Vector3(400, 0, 0), Vector3(-90, -180, -90))
+
+	#for x in range(-10, 10):
+	#	for z in range(-10, 10):
+	#		# Set the start wall.
+	#		Start.set_cell_item(x, 0, z, 0)
+	#		# Set the end wall.
+	#		End.set_cell_item(x, 0, z, 0)
+
+	# `x` = corridor length.
+	#for x in range(-2, 200):
+	#	for z in range(-10, 10):
+	#		# Set the floor.
+	#		Floor.set_cell_item(x, 0, z, 1)
+	#		# Set the ceiling.
+	#		Ceil.set_cell_item(x, 0, z, 1)
+
+	#	for z in range(-10, 10):
+	#			# Set the left wall.
+	#			Left.set_cell_item(x, 1, z, 0)
+	#			# Set the right wall.
+	#			Right.set_cell_item(x, 1, z, 0)
+
+	for x in range(-2, 10):
+		for z in range(-10, 10):
+			Floor.set_cell_item(x, 0, z, 1)
+
+	for x in range(-2, 10):
+		for z in range(-10, 10):
+			Floor.set_cell_item(x, 3, z, 1)
+
+	add_child(Floor)
+	add_child(Ceil)
+	#add_child(Left)
+	#add_child(Right)
+	#add_child(Start)
+	#add_child(End)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func new_grid(translation, rotation):
+	var grid = $DummyGridMap.duplicate()
+	grid.translation.x = translation.x
+	grid.translation.y = translation.y
+	grid.translation.z = translation.z
+	grid.rotation_degrees.x = rotation.x
+	grid.rotation_degrees.y = rotation.x
+	grid.rotation_degrees.z = rotation.z
+	grid.visible = true
+	return grid
