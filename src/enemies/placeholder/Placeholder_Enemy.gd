4de69ac3a8e12001b1d22986132025f1160da231
Ellen Marie Dash <me@duckie.co>
2019-04-24 05:31:36 -0400

    AAAAAAAA?

--- src/enemies/placeholder/Placeholder_Enemy.gd
+++ src/enemies/placeholder/Placeholder_Enemy.gd
@@ -1,11 +1,24 @@
 extends RigidBody
 
+const VIEW_DISTANCE = 20
+
 const MASS = 150
 
 var MAX_HEALTH = 100
 var health = 0
 
+var left_ray
+var right_ray
+
+var player_position_guess = Vector3(0, 0, 0)
+var last_left = null
+var last_right = null
+var last_left_time = 0
+var last_right_time = 0
+
 func _ready():
+	left_ray = $RayCast_Left
+	right_ray = $RayCast_Right
 	health = MAX_HEALTH
 	self.mode = MODE_CHARACTER
 	self.mass = MASS
@@ -13,10 +26,10 @@ func _ready():
 	set_max_contacts_reported(5)
 	connect("body_entered", self, "_process_body_entered")
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	pass
 
 func die():
 	get_tree().current_scene.mob_died(self)
@@ -53,3 +66,33 @@ func _process_body_entered(body):
 			force = impact_to_force(body, Vector3(vel.x, gravity.y, vel.z))
 			var damage = ceil(force / 200)
 			adjust_health(-damage)
+
+func found_player(raycast, player, position):
+	if raycast == left_ray:
+		last_left_time = OS.get_ticks_msec()
+		last_left = player
+		if last_right == player:
+			print("PLAYER IS GOING LEFT")
+	
+	if raycast == right_ray:
+		last_right_time = OS.get_ticks_msec()
+		last_right = player
+		if last_left == player:
+			print("PLAYER IS GOING RIGHT")
+
+	var diff = abs(last_left_time - last_right_time)
+	if diff > 100:
+		print("    Pretty sure they're still in front of me.")
+	else:
+		print("    They took " + str(diff) + " milliseconds.")
+	#print("  raycast  = " + str(raycast))
+	#print("  position = " + str(position))
+
+func search_for_player():
+	#print("SEARCHING FOR PLAYER")
+	pass
+
+func found_object(raycast, object, position):
+	print("FOUND OBJECT")
+	print("  object = " + str(object))
+	print("  position = " + str(position))
---
+++ src/enemies/placeholder/RayCast.gd
@@ -0,0 +1,19 @@
+extends RayCast
+
+var parent
+
+func _ready():
+	parent = get_parent()
+	set_cast_to(Vector3(parent.VIEW_DISTANCE, 0, 0))
+
+func _process(delta):
+	if not is_colliding():
+		parent.search_for_player()
+		return
+	
+	var collider = get_collider()
+	var position = collider.translation
+	if collider is KinematicBody:
+		parent.found_player(self, collider, position)
+	else:
+		parent.found_object(self, collider, position)
