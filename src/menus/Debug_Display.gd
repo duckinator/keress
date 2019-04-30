53d19d881e7938c0dd14c9d004bf84e73cfb8d7e
Ellen Marie Dash <me@duckie.co>
2019-04-30 09:49:21 -0400

    include frame time info instead of FPS, as it's more useful

--- src/menus/Debug_Display.gd
+++ src/menus/Debug_Display.gd
@@ -1,5 +1,9 @@
 extends Control
 
+# TODO: Determine at what point these become an actual problem.
+const FRAME_TIME_WARN = 0.1
+const PHYSICS_TIME_WARN = 0.1
+
 var os
 var engine
 
@@ -19,16 +23,28 @@ func _process(delta):
 		level = str(scene.current_level)
 		position = str(pos)
 	
+	var fps = str(Engine.get_frames_per_second())
+	var frame_time = Performance.get_monitor(Performance.TIME_PROCESS)
+	var physics_time = Performance.get_monitor(Performance.TIME_PHYSICS_PROCESS)
+	var ftime_str = "%.2f" % frame_time
+	var ptime_str = "%.2f" % physics_time
+	
+	if frame_time > FRAME_TIME_WARN:
+		ftime_str = "[color=red]" + ftime_str + "[/color]"
+	if physics_time > PHYSICS_TIME_WARN:
+		ptime_str = "[color=red]" + ptime_str + "[/color]"
+	
 	var lines = [
 		"System:   " + OS.get_name(),
 		"Engine:   Godot " + Engine.get_version_info()["string"],
-		"FPS:      " + str(Engine.get_frames_per_second()),
+		#"FPS:      " + fps,
+		"Timing:   frame=%s, physics=%s" % [ftime_str, ptime_str],
 		"Level:    " + level,
 		"Position: " + position,
 	]
 	
 	label.clear()
 	for idx in range(0, len(lines)):
-		label.add_text(lines[idx])
+		label.append_bbcode(lines[idx])
 		if idx < (len(lines) - 1):
 			label.newline()
