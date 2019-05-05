0412ee2af6120f1e871f8f9f16d615b9ee376df7
Ellen Marie Dash <me@duckie.co>
2019-05-05 16:23:35 -0400

    aaaaa?

--- src/levels/Level_001.gd
+++ src/levels/Level_001.gd
@@ -2,3 +2,6 @@ extends Spatial
 
 func _ready():
 	Game.spawn_player(self)
+
+func player_noise(trans, loudness):
+	Console.log("Player noise @ " + str(trans) + " with loudness of " + str(loudness))
