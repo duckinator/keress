9e6083e496e4b8a9554fd5d6f69002ae8b0a1129
Ellen Marie Dash <me@duckie.co>
2019-05-07 23:02:07 -0400

    Make sure to set the mouse to the correct mode when resuming.

--- src/Game.gd
+++ src/Game.gd
@@ -54,6 +54,7 @@ func pause():
 	self.pause_mode = PAUSE_MODE_PROCESS
 
 func resume():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
 	if popup != null:
 		popup.queue_free()
