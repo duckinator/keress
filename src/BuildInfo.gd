1e2c8a096b00d31878149752f306a264237f95f3
Ellen Marie Dash <me@duckie.co>
2019-09-05 23:56:43 -0400

    Actually print build info if it's available.

--- src/BuildInfo.gd
+++ src/BuildInfo.gd
@@ -1,13 +1,24 @@
 extends Node
 
 var defaults = {
-	"build_id": "(unknown)",
-	"build_hash": "(unknown)",
+	"build_id": null,
+	"build_hash": null,
 }
 
 onready var metadata = load_metadata()
 onready var build_id = metadata["build_id"]
 onready var build_hash = metadata["build_hash"]
+
+func _ready():
+	if build_id == null:
+		Console.log("Build ID not found. This is expected for unofficial builds.")
+	else:
+		Console.log("Build ID: " + build_id)
+	
+	if build_hash == null:
+		Console.log("No build information is available. This usually means it's run from the editor.")
+	else:
+		Console.log("Source for this build: https://github.com/duckinator/keress/tree/" + build_hash)
 
 func load_metadata():
 	var path = "res://build_info.json"
