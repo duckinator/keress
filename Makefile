6d81bb823796d3f0f80b7cbc01f3139402d19cc8
Ellen Marie Dash <me@duckie.co>
2019-09-06 00:56:40 -0400

    Include Cirrus CI URLs in logs.

--- Makefile
+++ Makefile
@@ -17,8 +17,7 @@ BUILD_HASH := $(shell git rev-parse --short HEAD)
 all: debug
 
 build_info:
-	env
-	echo "{\"build_id\": \""${BUILD_ID}"\", \"build_hash\": \"${BUILD_HASH}\"}" > src/build_info.json
+	echo "{\"build_id\": \""${BUILD_ID}"\", \"build_hash\": \"${BUILD_HASH}\", \"cirrus_task_id\": \""${CIRRUS_TASK_ID}"\"}" > src/build_info.json
 
 linux: build_info
 	mkdir -p build/linux
--- src/BuildInfo.gd
+++ src/BuildInfo.gd
@@ -3,22 +3,44 @@ extends Node
 var defaults = {
 	"build_id": null,
 	"build_hash": null,
+	"cirrus_task_id": null,
 }
 
 onready var metadata = load_metadata()
 onready var build_id = metadata.result["build_id"]
 onready var build_hash = metadata.result["build_hash"]
+onready var cirrus_task_id = metadata.result["cirrus_task_id"]
+
+var source_url = null
+var cirrus_url = metadata.result["cirrus_task_id"]
 
 func _ready():
-	if build_id == null:
-		Console.log("Build ID not found. This is expected for unofficial builds.")
-	else:
-		Console.log("Build ID: " + build_id)
+	normalize_metadata()
+	
+	if cirrus_task_id != null:
+		cirrus_url = "https://cirrus-ci.com/task/" + cirrus_task_id
+		Console.log("Build logs: " + cirrus_url)
+	
+	#if build_id == null:
+	#	Console.log("Build ID not found. This is expected for unofficial builds.")
+	#else:
+	#	Console.log("Build ID: " + build_id)
 	
 	if build_hash == null:
 		Console.log("No build information is available. This usually means it's run from the editor.")
 	else:
-		Console.log("Source for this build: https://github.com/duckinator/keress/tree/" + build_hash)
+		source_url = "https://github.com/duckinator/keress/tree/" + build_hash
+		Console.log("Source for this build: " + source_url)
+
+func normalize_metadata():
+	if build_id == "":
+		build_id = null
+		
+	if build_hash == "":
+		build_hash = null
+	
+	if cirrus_task_id == "":
+		cirrus_task_id = null
 
 func load_metadata():
 	var path = "res://build_info.json"
