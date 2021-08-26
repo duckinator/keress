838c5b7851529c3678da3551420d98b63dceab7c
Ellen Marie Dash <me@duckie.co>
2021-08-25 20:30:53 -0400

    Avoid using Enemies.spawn_horde(); that system is broken and being removed.

--- src/levels/Level_002.gd
+++ src/levels/Level_002.gd
@@ -2,10 +2,11 @@ extends Spatial
 
 var mobs = []
 func _ready():
-	Enemies.spawn(Vector3(-30, 30, -40))
-	Enemies.spawn(Vector3(0, 30, -10))
-	Enemies.spawn(Vector3(0, 30, -65))
-	Enemies.spawn(Vector3(30, 30, -40))
+	#Enemies.spawn(Vector3(-30, 30, -40))
+	#Enemies.spawn(Vector3(0, 30, -10))
+	#Enemies.spawn(Vector3(0, 30, -65))
+	#Enemies.spawn(Vector3(30, 30, -40))
+	pass
 
 func update_door_locks():
 	if len(mobs) == 0:
--- src/levels/Level_003.gd
+++ src/levels/Level_003.gd
@@ -3,16 +3,17 @@ extends Spatial
 var mobs = []
 func _ready():
 	# $Platform
-	Enemies.spawn_horde(Vector3(-56, 3, 40), Vector2(30, 100), 3)
-	Enemies.spawn_horde(Vector3(-56, 3, -180), Vector2(30, 100), 3)
+	#Enemies.spawn_horde(Vector3(-56, 3, 40), Vector2(30, 100), 3)
+	#Enemies.spawn_horde(Vector3(-56, 3, -180), Vector2(30, 100), 3)
 	
 	# $Platform_Left
-	Enemies.spawn_horde(Vector3(-370, -27, 150), Vector2(280, 30), 20)
+	#Enemies.spawn_horde(Vector3(-370, -27, 150), Vector2(280, 30), 20)
 	# $Platform_Right
-	Enemies.spawn_horde(Vector3(-370, -27, -180), Vector2(280, 30), 20)
+	#Enemies.spawn_horde(Vector3(-370, -27, -180), Vector2(280, 30), 20)
 	
 	# $Platform_Mid
-	Enemies.spawn_horde(Vector3(-220, -55, -182), Vector2(30, 400), 16)
+	#Enemies.spawn_horde(Vector3(-220, -55, -182), Vector2(30, 400), 16)
+	pass
 
 func update_door_locks():
 	if len(mobs) == 0:
