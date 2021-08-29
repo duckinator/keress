ea75c3b6616ca58eeec3bb27f55af915e44529b7
Ellen Marie Dash <me@duckie.co>
2021-08-29 16:36:32 -0400

    lots of refactoring, improved debug info, etc

--- src/Enemies.gd
+++ src/Enemies.gd
@@ -1,7 +1,6 @@
 extends Node
 
 func spawn(pos):
-	Console.log("Enemies.spawn()")
 	var scene = load("res://enemies/enemy1/Enemy1.tscn").instance()
 	scene.translate(pos)
 	get_tree().current_scene.add_child(scene)
--- src/enemies/enemy1/Enemy1.gd
+++ src/enemies/enemy1/Enemy1.gd
@@ -1,12 +1,13 @@
 extends RigidBody
 
-enum { IDLE, CHASE, SEARCH, ATTACK, EVADE }
+enum { IDLE, SEARCH, ATTACK, EVADE }
+const STATE_STRS = {IDLE: "IDLE", SEARCH: "SEARCH", ATTACK: "ATTACK", EVADE: "EVADE"}
 
 const MAX_SPEED = 50
 const MAX_HEALTH = 140
 const MASS = 80
 
-const BACKOFF = 3
+const BACKOFF = 5
 
 # How many times more damage is dealt the mob slams into a floor.
 const FALL_DAMAGE_MULTIPLIER = MAX_HEALTH / 100.0
@@ -45,24 +46,23 @@ func _ready():
 	self.mass = MASS
 	self.mode = MODE_CHARACTER
 	
+	$Viewport/VBoxContainer/Health.max_value = MAX_HEALTH
+	
 	map = get_tree().current_scene
 	player = map.get_node('Player')
 	
-	set_contact_monitor(true)
-	set_max_contacts_reported(5)
-	var err = connect("body_entered", self, "_process_body_entered")
-	if err != OK:
-		Console.log(err)
+	#set_contact_monitor(true)
+	#set_max_contacts_reported(5)
+	#var err = connect("body_entered", self, "_process_body_entered")
+	#if err != OK:
+	#	Console.log(err)
 	Noise.add_listener(self, "heard_noise")
 	AreasOfInterest.add_mob(self)
 	
 	map.mobs.append(self)
-	
-	$Sprite3D.set_texture($Viewport.get_texture())
-	$Viewport/Label.text = self.name
 
 func get_last_velocity():
-	return linear_velocity
+	return last_velocity
 
 func set_target(new_target):
 	last_target = target
@@ -72,27 +72,43 @@ func get_target():
 	return target
 
 func set_state(new_state):
+	$Sprite3D.set_texture($Viewport.get_texture())
+	$Viewport/VBoxContainer/Label.text = self.name + "(" + STATE_STRS[new_state] + ")"
+	
 	last_state = state
 	state = new_state
+	
+	if state == EVADE and state != last_state:
+		reset_evade()
 
 func jump(assist=1):
 	$Below.force_raycast_update()
 	if $Below.is_colliding():
 		apply_central_impulse(Vector3.UP * (1000 * assist))
 
+const BELOW_WORLD_DAMAGE = MAX_HEALTH / 10
+
 var current = 0.0
 func _process(delta):
+	$Sprite3D.visible = Debug.mob_debug_enabled
+	
 	current += delta
 	if current < 0.5:
 		return
 	
-	if see_player() or near_player() and not (state == ATTACK or state == EVADE):
+	if translation.y < -5:
+		Console.log(name + " taking " + str(BELOW_WORLD_DAMAGE) + " damage from being below the world. :(")
+		adjust_health(-BELOW_WORLD_DAMAGE)
+	
+	if see_player() or near_player():
 		target = player.translation
-		set_state(SEARCH)
+		if not (state == ATTACK or state == EVADE):
+			set_state(SEARCH)
 	
 	if target != null and target.y < 0:
 		target.y = 0
 	
+
 	match state:
 		IDLE:
 			idle(delta)
@@ -116,6 +132,11 @@ func near_player():
 		return false
 	return distance_to_player() < 20
 
+func uncomfortably_close_to_player():
+	if player == null:
+		return false
+	return distance_to_player() < 2
+
 func see_player():
 	if player == null:
 		return false
@@ -127,35 +148,57 @@ func see_player():
 
 func investigate(trans):
 	Console.log("MOB " + str(self) + " INSTRUCTED TO CHECK " + str(trans))
-	target = trans
-	set_state(SEARCH)
+	if not near_player():
+		target = trans
+		set_state(SEARCH)
 
 func idle(_delta):
 	pass
 
-#var chase_current = 0.0
-func chase(delta, backoff):
-	if target == null or state != SEARCH:
+func chase(delta, backoff, offset=null):
+	if target == null:
 		return
 	
-	var offset = Vector3(rand_range(-backoff, backoff), 0, rand_range(-backoff, backoff))
-	
+	if offset == null:
+		offset = Vector3(rand_range(-backoff, backoff), 0, rand_range(-backoff, backoff))
 	var cur_target = target + offset
+	Console.log(STATE_STRS[state] + "; " + str(cur_target))
 	
-	var velocity = translation.direction_to(cur_target) * 100
-	#Console.log("chase(" + str(delta) + ", " + str(backoff) + "); velocity = " + str(velocity))
+	var velocity = translation.direction_to(cur_target) * 75
+	#Console.log(STATE_STRS[state] + "/chase(" + str(delta) + ", " + str(backoff) + "); velocity=" + str(velocity) + "; offset=" + str(offset))
 	apply_central_impulse(velocity)
 	last_velocity = velocity
+	
+	if distance_to_player() < 4:
+		var new_target = player.translation
+		new_target.x += pow(-1, randi() % 2) * 6
+		new_target.z += pow(-1, randi() % 2) * 6
+		Console.log(new_target)
+		target = new_target
+	
+	if uncomfortably_close_to_player():
+		# If they are on top of us, then they shouldn't take damage.
+		var diff = player.translation.y - translation.y
+		if diff < 1:
+			Console.log("BAM")
+			player.adjust_health(-5)
 
 func search(delta):
-	chase(delta, BACKOFF - 2)
-	if translation.distance_to(target) <= BACKOFF:
-		set_state(ATTACK)
-		return
+	chase(delta, 0)
 
+const ATTACK_DURATION = 1
+var attack_duration = 0.0
 const ATTACK_DELAY = 0.5
 var attack_delta = 0.0
 func attack(delta):
+	attack_duration += delta
+	
+	if attack_duration > ATTACK_DURATION:
+		attack_duration = 0.0
+		attack_delta = 0.0
+		set_state(EVADE)
+		return
+	
 	if attack_delta < ATTACK_DELAY:
 		attack_delta += delta
 		return
@@ -166,14 +209,47 @@ func attack(delta):
 		return
 	
 	chase(delta, 0)
-	
-	if distance_to_player() < 4:
-		Console.log("PEW PEW")
-		player.adjust_health(-5)
+
+
+const DIRECTION_CHANGE_PERIOD = 3.0
+var direction_change_current = 0.0
+var direction = 1
+const EVADE_PERIOD = 0.5 # 0.5s between each movement.
+var evade_current = 0.0
+var evade_offset = Vector3(0, 0, 0)
+var evade_angle = 0
+func reset_evade():
+	direction_change_current = 0.0
+	direction = 1
+	evade_current = 0.0
+	evade_offset = Vector3(0, 0, 0)
+	evade_angle = 0
 
 func evade(delta):
-	#Console.log(str(self) + " EVADE")
-	chase(delta, 16)
+	if direction_change_current < DIRECTION_CHANGE_PERIOD:
+		direction_change_current += delta
+	else:
+		direction_change_current = 0
+		direction *= -1
+	
+	if evade_current < EVADE_PERIOD:
+		evade_current += delta
+		evade_angle += rand_range(5, 15) * direction # degrees to rotate by
+		if evade_angle >= 360:
+			evade_angle -= 360
+	else:
+		var radius = 8 # Stay 4m away.
+	
+		var evade_radians = evade_angle * 0.0174532925
+	
+		var x = radius * cos(evade_radians)
+		var z = radius * sin(evade_radians)
+		
+		evade_offset = Vector3(x, 0, z)
+		Console.log("Applying offset: " + str(evade_offset))
+		evade_current = 0
+	
+	chase(delta, BACKOFF, evade_offset)
 
 
 func raycast_name(raycast):
@@ -205,9 +281,8 @@ func cleanup():
 	queue_free()
 
 func adjust_health(value):
-	AreasOfInterest.add_aoi(translation)
 	health = clamp(round(health + value), 0, MAX_HEALTH)
-	Console.debug(str(self) + ".health = " + str(health))
+	$Viewport/VBoxContainer/Health.value = health
 	if health <= 0:
 		die()
 
@@ -215,57 +290,68 @@ func adjust_health(value):
 
 
 
-func vel_to_speed(vel):
-	var x = abs(vel.x)
-	var y = abs(vel.y)
-	var z = abs(vel.z)
-	return sqrt((x * x) + (y * y) + (z * z))
-
-func impact_to_force(collider, collider_vel):
-	var speed = vel_to_speed(collider_vel)
-	return collider.MASS * speed
-
-func force_to_damage(force):
-	return ceil(force / 400)
-
-func impact_to_damage(collider, collider_vel):
-	var force = impact_to_force(collider, collider_vel)
-	return force_to_damage(force)
-
-
-var last_body = null
-func _process_body_entered(body):	
-	# Fall damage.
-	if body is StaticBody:
-		if last_y == null:
-			return
-		var diff = abs(translation.y - last_y)
-		if diff > 1:
-			adjust_health(-diff * FALL_DAMAGE_MULTIPLIER)
-		last_y = translation.y
-		return
-	
-	if not body.has_method("get_last_velocity"):
-		return
-	
-	#var height = $MeshInstance.mesh.height
-	if floor(body.translation.y) > floor(translation.y):
-		# Curb stomps.
-		var vel = body.get_last_velocity()
-		var damage = impact_to_damage(body, vel) * 10
-		adjust_health(-damage)
-	else:
-		# Less dramatic impacts.
-		var vel = body.get_last_velocity()
-		var damage = impact_to_damage(body, vel) / 10
-		adjust_health(-damage)
-	
-	if body.has_method("jump"):
-		body.jump()
-	
-	var is_player = body is KinematicBody and body.name == "Player"
-	var is_enemy = body is RigidBody and body.name.begins_with("Enemy")
-	if is_enemy or is_player:
-		target = body.translation
-	set_state(EVADE)
-	last_body = body
+#func vel_to_speed(vel):
+#	var x = abs(vel.x)
+#	var y = abs(vel.y)
+#	var z = abs(vel.z)
+#	return sqrt((x * x) + (y * y) + (z * z))
+#
+#func impact_to_force(collider, collider_vel):
+#	var speed = vel_to_speed(collider_vel)
+#	return collider.MASS * speed
+#
+#func force_to_damage(force):
+#	return ceil(force / 400)
+#
+#func impact_to_damage(collider, collider_vel):
+#	var force = impact_to_force(collider, collider_vel)
+#	return force_to_damage(force)
+#
+#
+#var last_body = null
+#func _process_body_entered(body):
+#	# Fall damage.
+#	if body is StaticBody:
+#		if last_y == null:
+#			return
+#		var diff = abs(translation.y - last_y)
+#		if diff > 1:
+#			adjust_health(-diff * FALL_DAMAGE_MULTIPLIER)
+#		last_y = translation.y
+#		return
+#
+#	if not body.has_method("get_last_velocity"):
+#		return
+#
+#	#var height = $MeshInstance.mesh.height
+#	if floor(body.translation.y) > floor(translation.y):
+#		# Curb stomps.
+#		var vel = body.get_last_velocity()
+#		var damage = impact_to_damage(body, vel) * 10
+#		adjust_health(-damage)
+#	else:
+#		# Less dramatic impacts.
+#		var vel = body.get_last_velocity()
+#		var damage = impact_to_damage(body, vel) / 10
+#		adjust_health(-damage)
+#
+#	var is_player = body is KinematicBody and body.name == "Player"
+#	var is_enemy = body is RigidBody and body.name.begins_with("Enemy")
+#	if is_enemy or is_player:
+#		target = body.translation
+#
+#		# If they are on top of us, then they shouldn't take damage.
+#		var diff = body.translation.y - translation.y
+#		if diff < 1:
+#			Console.log("BAM")
+#			body.adjust_health(-5)
+#
+#	if is_player:
+#		var force = get_last_velocity()
+#	#	Console.log("force = " + str(force))
+#		jump()
+#
+#	if body.has_method("jump") and not is_player:
+#		body.jump()
+#	set_state(EVADE)
+#	last_body = body
