3249e82340a31ced61961a69b3531cd479eb6e52
Ellen Marie Dash <me@duckie.co>
2021-09-15 17:33:39 -0400

    Remove Level_001.gd/_process()

--- src/levels/Level_001.gd
+++ src/levels/Level_001.gd
@@ -7,9 +7,3 @@ func _ready():
 	#Enemies.Enemy1.spawn(Vector3(0, 3, -12))
 	#Enemies.Enemy1.spawn(Vector3(-10, 3, -12))
 	#Enemies.Enemy1.spawn(Vector3(-15, 3, -14))
-
-var won = false
-func _process(delta):
-	if not won and len(Game.mobs) == 0:
-		won = true
-		Console.log("woo!")
