eb467c20a9a98a9f26bf20fe39757374a13c3b56
Ellen Marie Dash <me@duckie.co>
2021-09-15 12:59:39 -0400

    Attempt to make enemy spawning more generic i guess idk fuck this part of the codebase is weird

--- src/Enemies.gd
+++ src/Enemies.gd
@@ -1,6 +1,11 @@
 extends Node
 
-func spawn(pos):
-	var scene = load("res://enemies/enemy1/Enemy1.tscn").instance()
+func _spawn(name, pos):
+	var capitalized_name = name.capitalize()
+	var scene = load("res://enemies/" + name + "/" + capitalized_name + ".tscn").instance()
 	scene.translate(pos)
 	get_tree().current_scene.add_child(scene)
+
+class Enemy1:
+	static func spawn(pos):
+		Enemies._spawn("enemy1/Enemy1", pos)
