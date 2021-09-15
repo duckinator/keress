512933195d031fd36ad726c2383d35c437c5da8f
Ellen Marie Dash <me@duckie.co>
2021-09-15 13:12:09 -0400

    fix src/Enemies.gd.

--- src/Enemies.gd
+++ src/Enemies.gd
@@ -1,11 +1,12 @@
 extends Node
 
 func _spawn(name, pos):
-	var capitalized_name = name.capitalize()
+	var capitalized_name = name
+	capitalized_name[0] = capitalized_name[0].to_upper()
 	var scene = load("res://enemies/" + name + "/" + capitalized_name + ".tscn").instance()
 	scene.translate(pos)
 	get_tree().current_scene.add_child(scene)
 
 class Enemy1:
 	static func spawn(pos):
-		Enemies._spawn("enemy1/Enemy1", pos)
+		Enemies._spawn("enemy1", pos)
